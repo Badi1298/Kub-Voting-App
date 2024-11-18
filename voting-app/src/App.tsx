072af4fa3344befa6bb22037a679c33781bcce6d
Serban <serban.david@flowmatters.com>
2024-11-18 12:19:19 +0100

    voting app buttons

--- voting-app/src/App.tsx
+++ voting-app/src/App.tsx
@@ -1,15 +1,15 @@
-import './App.css'
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <h1>Choose between:</h1>
-      <ul>
-        <li>Dogs</li>
-        <li>Cats</li>
-      </ul>
-    </>
-  )
+	return (
+		<>
+			<h1>Choose between:</h1>
+			<div className='btn-container'>
+				<button>Dogs</button>
+				<button>Cats</button>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
