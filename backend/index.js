d90d72ce40ef10171a731f188996152f9f900090
Serban <serban.david@flowmatters.com>
2024-11-18 16:20:27 +0100

    redis BE

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+const cors = require('cors');
+const redis = require('redis');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const client = redis.createClient();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+client.on('connect', () => {
+	console.log('Connected to Redis server successfully!');
+});
+
+client.on('error', (error) => {
+	console.error('Redis client encountered an error:', error);
+});
+
+(async () => {
+	await client.connect();
+})();
+
+app.get('/data', async (req, res) => {
+	const value = await client.get('dogs');
+	res.json({ value });
+});
+
+app.post('/data', async (req, res) => {
+	const { key, value } = req.body;
+	await client.set(key, value);
+
+	res.json({ key, value });
+});
+
+app.listen(8001, () => {
+	console.log('Server running on port 8001');
+});
--- voting-app/src/App.tsx
+++ voting-app/src/App.tsx
@@ -1,11 +1,34 @@
+import { useEffect } from 'react';
+
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+	useEffect(() => {
+		async function getRedisData() {
+			const response = await axios.get('http://localhost:8001/data');
+
+			return response;
+		}
+
+		getRedisData();
+	}, []);
+
+	const voteDogs = async () => {
+		const response = await axios.post('http://localhost:8001/data', {
+			key: 'dogs',
+			value: 3,
+		});
+
+		console.log(response);
+	};
+
 	return (
 		<>
 			<h1>Choose between:</h1>
-			<div className='btn-container'>
-				<button>Dogs</button>
+			<div className="btn-container">
+				<button onClick={voteDogs}>Dogs</button>
 				<button>Cats</button>
 			</div>
 		</>
